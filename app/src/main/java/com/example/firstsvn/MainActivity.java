9a8175487e0f23e989e83409cf369af582647a44
lifukai0214 <www.lifukai0214@163.com>
2020-12-10 16:55:11 +0800

    是一个代码

---
+++ app/src/main/java/com/example/firstsvn/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.firstsvn;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String asd = "q21321we";
+        String qwe = "a123";
+        String tl="测试";
+    }
+}
